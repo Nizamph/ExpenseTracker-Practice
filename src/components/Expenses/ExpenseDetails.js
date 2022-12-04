e042d3fc03d472c67f5611f608aa3813728bb7de
Nizam <nizamph&@gmail.com>
2022-12-04 20:18:08 +0530

    amount is also updated along with title

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -5,11 +5,13 @@ import { useState } from "react"
 const ExpenseDetails = (props) => {
      
          const [title,setTitle]=useState(props.title)
+         const [amount,setAmount]=useState(props.amount)
 
          console.log('react is updated')
 
      const ClickHandler = () => {
          setTitle('updated')
+         setAmount(100)
          console.log(title)
      }
 
@@ -18,13 +20,13 @@ const ExpenseDetails = (props) => {
      }
 
 return(
-     <div id="exp" className="expense-item__description">
+     <div className="expense-item__description">
      
     <h2>{title}</h2>
     <h3>{props.location}</h3>
     
     
-     <div id="amount" className="expense-item__price">${props.amount}</div>
+     <div className="expense-item__price">${amount}</div>
      <button onClick={ClickHandler}>Change title</button>
      <button onClick={DeleteHandler}>Delete</button>
      </div>
